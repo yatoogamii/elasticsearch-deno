6e7290f3b89272935348d8facd8feadaffb04cff
yatoogamii <bracaval.elias.pro@gmail.com>
2020-12-12 16:37:17 +0100

    removing interface. removing queryParams of get method

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,3 @@
-interface SearchParams {
-  /** https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html#search-search-api-query-params */
-  queryParameters?: URLSearchParams;
-
-  /** https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html#search-search-api-request-body */
-  body?: BodyInit;
-}
-
 /** Class representing an elasticsearch client. */
 export class Client {
   readonly url: URL;
@@ -19,37 +11,17 @@ export class Client {
   this.url = new URL(host, index);
   }
 
-  async get(params?: SearchParams) {
-    const url = new URL(`${this.url}/_search?${params?.queryParameters ?? ""}`);
+  async get(body?: JSON) {
+    const url = new URL(`${this.url}/_search?`);
 
     const response = await fetch(url.toString(), {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(params?.body),
+      body: JSON.stringify(body),
     });
 
     return response.json();
   }
-
-  /**
-   * Returns search hits that match the query defined in the request.
-   * https://www.elastic.co/guide/en/elasticsearch/reference/current/search-search.html#search-search
-   * @param {SearchParams} p
-   */
-  // async search(p: SearchParams = { method: "GET", index: "" }) {
-  //   const url = new URL(
-  //     `${this.url}/${p.index}/_search?${p.queryParameters ?? ""}`
-  //   );
-  //   const resp = await fetch(url, {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     method: p.method,
-  //     body: JSON.stringify(p.body),
-  //   });
-
-  //   return resp.json();
-  // }
 }
